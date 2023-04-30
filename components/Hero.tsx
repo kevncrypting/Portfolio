9ba25f63feb7f38972823ff07cbced417b30f3fd
Thanuseng Kevin Prachith <tkprachith@gmail.com>
2023-04-29 23:52:09 -0400

    Edits hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
     <section className="md:flex">
       <div>
 
-      <span>Hi, my name is</span>
+      <span>Hi, my name is...</span>
       <h1 className="font-bold">Thanuseng Kevin Prachith</h1>
       <h2 className="font-semibold">Full Stack Web Developer</h2>
       <p>I am a software engineer with a passion for creating aesthetic and accessible products. Currently, I&apos;m learning more about the inner workings of Next.js and building out my personal blog.</p>
