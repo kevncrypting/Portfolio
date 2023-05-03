c19f16fc22a13fdd3c41fa478a9fcbddfd029fd9
Thanuseng Kevin Prachith <tkprachith@gmail.com>
2023-05-02 23:15:29 -0400

    Adds a few skills to about section

--- components/About.tsx
+++ components/About.tsx
@@ -2,6 +2,14 @@ export default function About() {
     return (
         <>
         <div>About</div>
+        Skills
+        <ul>
+            <li>HTML, CSS, Javascript</li>
+            <li></li>
+            <li></li>
+            <li>React</li>
+            <li>Sequelize, PostgresQL, MongoDB</li>
+        </ul>
         </>
     )
 }
