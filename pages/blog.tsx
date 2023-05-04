b01b7f0f6db912b90285575ee02f3dc9c8c94051
Thanuseng Kevin Prachith <tkprachith@gmail.com>
2023-05-04 08:37:16 -0400

    Modifies blog page

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -3,7 +3,7 @@ import Layout from "@/components/Layout";
 export default function Blog() {
   return (
     <Layout titleAddition=" | Blog">
-      <h1>Welcome to my blog</h1>
+      <h1>Welcome to my blog!</h1>
     </Layout>
   );
 }
